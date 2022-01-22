9e425de88f019a80fb73d758a90f3573beb5c35c
HongxuanG <1359774872@qq.com>
2022-01-22 10:14:39 +0800

    add cases

---
+++ ts-challenges/4-easy-pick/template.ts
@@ -0,0 +1,31 @@
+
+
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key]
+}
+
+
+
+
+// function myPick(todo, keys) {
+
+//   let obj = {}
+//   keys.forEach(key => {
+//     if (key in todo) {
+//       obj[key] = todo[key]
+//     }
+//   });
+//   return obj
+// }
+
+// 1.返回一个对象
+// 2.遍历第二个参数keys
+// 3.取值 todo[key]
+// 4.看看key在不在todo里面
+
+
+// 所用知识点
+// 遍历联合类型 mapped
+// --https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+// 访问类型 indexed
+// --https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
---
+++ ts-challenges/7-easy-readonly/template.ts
@@ -0,0 +1,31 @@
+type MyReadonly<T> = {
+  readonly [key in keyof T]: T[key]
+}
+
+// interface TODO{
+//   a: string;
+//   b: string;
+//   c: string;
+// }
+// type r = keyof TODO
+// const rv: r = 'a'
+
+// js 的代码逻辑
+// function myReadonly(Obj) {
+//   const result = {}
+//   for (const key in Obj) {
+//     result['readonly'+key] = Obj[key]
+//   }
+//   return result
+// }
+
+// 返回一个对象
+// 遍历传进来的obj
+// 让每个Obj的property都加上readonly关键字
+// 通过key获取Obj里面的值
+
+// 所用知识
+// 1.keyof关键字
+// 2.readonly关键字
+// 3.mapped 映射类型
+// 4.indexed 索引类型
