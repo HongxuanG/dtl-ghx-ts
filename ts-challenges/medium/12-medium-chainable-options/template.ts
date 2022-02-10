2818c87cf9def458f13941fec8f517f9d0a82e2f
HongxuanG <1359774872@qq.com>
2022-02-10 14:41:02 +0800

    每天一题

---
+++ ts-challenges/medium/12-medium-chainable-options/template.ts
@@ -0,0 +1,17 @@
+type Chainable<T extends { [key in keyof T]: T[key] } = {}> = {
+  option<K extends Omit<T, keyof T> & string, V>(key: Exclude<K, keyof T>, value: V): Chainable<T & Record<K, V>>
+  get(): Omit<T, 'option' | 'get'>
+}
+declare const a: Chainable
+
+const result1 = a
+  .option('foo', 123)
+  .option('bar', { value: 'Hello World' })
+  .option('name', 'type-challenges')
+  .get()
+
+console.log(result1)
+
+// key: Exclude<K, keyof T 用于排除 @ts-expect-error的问题
+// Omit 构造一个具有T属性的类型，除了K类型中的属性。
+// Exclude 从T中排除那些可赋给U的类型。
