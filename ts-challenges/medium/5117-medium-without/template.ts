32b23870e2d1b929614d03b5351b2ca952b2333d
HongxuanG <1359774872@qq.com>
2022-02-28 13:58:55 +0800

    每日三体

---
+++ ts-challenges/medium/5117-medium-without/template.ts
@@ -0,0 +1,24 @@
+type MyTupleToUnion<T extends unknown[] = []> = T[number]
+
+type UnionToIntersection<U> =
+  (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+
+type LastOf<T> =
+  UnionToIntersection<T extends any ? () => T : never> extends () => (infer R) ? R : never
+
+// TS4.0+
+type MyPush<T extends any[], V> = [...T, V];
+
+// Tuple 转 Union
+type TuplifyUnion<T, L = LastOf<T>, N = [T] extends [never] ? true : false> =
+  true extends N ? [] : MyPush<TuplifyUnion<Exclude<T, L>>, L>
+
+/**
+ * 先各自转成Union然后Exclude 
+ * 再转回Tuple
+ */
+type Without<T extends unknown[], U> =
+  U extends unknown[]
+  ? TuplifyUnion<Exclude<MyTupleToUnion<T>, MyTupleToUnion<U>>>
+  : Without<T, [U]>
+
---
+++ ts-challenges/medium/5140-medium-trunc/template.ts
@@ -0,0 +1,2 @@
+
+type Trunc<S extends string | number> = `${S}` extends `${infer Prefix}.${string}` ? Prefix : `${S}`
