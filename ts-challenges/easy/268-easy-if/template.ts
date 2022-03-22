41f5d557214de5720bd49cc29f6e3c7dedd613ca
HongxuanG <1359774872@qq.com>
2022-03-22 18:05:14 +0800

    修正小错误

--- ts-challenges/easy/268-easy-if/template.ts
+++ ts-challenges/easy/268-easy-if/template.ts
@@ -1,4 +1,4 @@
-type If<C extends Boolean, T, F> = C extends true ? T : F
+type If<C extends boolean, T, F> = C extends true ? T : F
 
 // function IIf(boolean: Boolean, truthy: any, falsy: any) {
 //   if (boolean) {
